7982a8778711e5396d31ff3a79f3aa6d52ddceb9
Maria Isabel <marie.antoinette18@gmail.com>
2025-10-25 21:55:59 -0300

    actualizacion carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,15 +1,42 @@
+import { useContext, useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
-import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import "../styles/styles.css";
 
 export default function CartWidget() {
-  const { cartQuantity } = useContext(CartContext);
+  const { cart, cartQuantity } = useContext(CartContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="cart-widget">
-      <FaShoppingCart />
-      <span>{cartQuantity}</span>
+    <div
+      className="cart-widget"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
+      <FaShoppingCart className="cart-icon" />
+      {cartQuantity > 0 && <span className="cart-count">{cartQuantity}</span>}
+
+      {isOpen && (
+        <div className="cart-dropdown">
+          {cart.length === 0 ? (
+            <p>Tu carrito está vacío 🛍</p>
+          ) : (
+            <ul>
+              {cart.map((item) => (
+                <li key={item.id}>
+                  <img src={item.img} alt={item.name} />
+                  <div>
+                    <p>{item.name}</p>
+                    <p>
+                      {item.quantity} × ${item.price}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )}
     </div>
   );
 }
